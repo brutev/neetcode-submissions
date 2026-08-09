7b2feafbd90ab800745cc37ec6d15d262600ddfa
Vignesh V <98809506+brutev@users.noreply.github.com>
2026-08-09 10:22:00 +0530

    Add: python-set-operations - submission-0

---
+++ Python For Beginners/python-set-operations/submission-0.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def count_unique_words(words: List[str]) -> int:
+    if len(words) <= 0:
+        return 0
+    else:
+        my_set = set(words)
+        test = len(my_set)
+    return test
+
+
+# do not modify code below this line
+print(count_unique_words(["hello", "world", "hello", "goodbye"]))
+print(count_unique_words(["hello", "world", "i", "am", "world"]))
+print(count_unique_words(["hello", "hello", "hello"]))
+print(count_unique_words([]))
